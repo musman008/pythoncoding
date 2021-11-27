d83c9ae0a7490778c8ad46bb2ab8b09a7ea7d3cd
musman008 <1muhammad.usman@gmail.com>
2021-11-27 13:02:05 +0500

    jk

---
+++ print.py
@@ -0,0 +1,18 @@
+lst  = [23,55,2,2,5,77]
+print(lst)
+
+
+
+# print(lst[1-6])
+# print(lst[1:])
+lst.append(100)
+lst.insert(2,200)
+lst.pop()
+del lst[3]
+print(lst)
+
+# lst.clear()
+# print(lst)
+
+if (lst[1]>23):
+    print("greater than 23")
